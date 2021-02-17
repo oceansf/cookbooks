281f39575aa20637034bb0fd2963b99c4006fb04
Ocean Fuaga <oceansfuaga@gmail.com>
2021-02-17 14:42:54 -0800

    .gitignore Fixed

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGO_URI:
-    "mongodb+srv://oceansf:VocatusAtque4!@cluster0.3gd3r.mongodb.net/cookbooks?retryWrites=true&w=majority",
-  JWT_SECRET: "randomstring",
-  SENDGRID_API:
-    "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",
-  SITE_URL: "http://localhost:3000",
-};
